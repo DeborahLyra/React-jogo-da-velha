a59c62f946ef3813b49ba9cda9a18b6f4ece1342
Deborah Lyra <deborahp.lyra@gmail.com>
2022-09-29 14:58:29 -0300

    Criado estados no gameOption

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,24 +1,25 @@
+import { useState } from 'react'
+
 import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOpition'
 
 function Game (){
+
+  const [gameState, setGameState] = useState( Array(9).fill(0))
+
   return (
     <div className={styles.game}>
-      <GameOption/> 
-      <GameOption/> 
-      <GameOption/> 
-      <GameOption/> 
-      <GameOption/> 
-      <GameOption/> 
-      <GameOption/> 
-      <GameOption/> 
-      <GameOption/> 
-
+      {
+        gameState.map((value, position)=> 
+        <GameOption 
+          key ={`game-option-pos-${position}`}
+          status ={value}
+        /> 
+        )
+      }
     </div>
   )
-
-
 }
 
 export default Game
